8dc3930f606a2c8a2236b73b015098644e387584
srajsonu <43319626+srajsonu@users.noreply.github.com>
2019-01-08 20:04:53 +0530

    Initial Commit

---
+++ whatAnimal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whatAnimal
+//
+//  Created by ARY@N on 08/01/19.
+//  Copyright © 2019 ARYAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
